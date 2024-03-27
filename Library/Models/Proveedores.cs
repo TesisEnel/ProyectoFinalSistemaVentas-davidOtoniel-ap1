0db9efea69d6efd22044b10eb86811297abe1bdd
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-26 22:16:20 -0400

    Se modificaron los límites de la longitud de los caracteres y se agregó el atributo Ciudad.

--- Library/Models/Proveedores.cs
+++ Library/Models/Proveedores.cs
@@ -18,16 +18,20 @@ public class Proveedores
 
 	[Required(ErrorMessage = "Debe ingresar un nombre.")]
 	[RegularExpression(@"^[a-zA-ZñÑ\s]+$", ErrorMessage = "Este campo no acepta números ni caracteres especiales.")]
-	[StringLength(15, ErrorMessage = "El nombre es demasiado largo, el límite es de 15 caracteres.")]
+	[StringLength(25, ErrorMessage = "El límite es de 25 caracteres.")]
 	public string Nombre { get; set; }
 
 	[Required(ErrorMessage = "Debe ingresar una dirección.")]
-	[StringLength(70, ErrorMessage = "La dirección es demasiado larga, el límite es de 70 caracteres.")]
+	[StringLength(70, ErrorMessage = "El límite es de 70 caracteres.")]
 	public string Direccion { get; set; }
+
+	[Required(ErrorMessage = "Debe ingresar una ciudad.")]
+	[StringLength(30, ErrorMessage = "El límite es de 30 caracteres.")]
+	public string Ciudad { get; set; }
 
 	[Required(ErrorMessage = "Debe ingresar un email")]
 	[EmailAddress(ErrorMessage = "El formato para el email no es válido.")]
-	[StringLength(40, ErrorMessage = "La email es demasiado largo, el límite es de 40 caracteres.")]
+	[StringLength(40, ErrorMessage = "El límite es de 40 caracteres.")]
 	public string Email { get; set; }
 
 	[Required(ErrorMessage = "Debe elegir un tipo de contribuyente.")]
@@ -37,7 +41,7 @@ public class Proveedores
 	[RegularExpression(@"^[0-9]{11}$", ErrorMessage = "El RNC debe tener exactamente 11 dígitos numéricos.")]
 	public string RNC { get; set; }
 
-	[StringLength(250, ErrorMessage = "La nota es demasiado larga, el límite es de 250 caracteres.")]
+	[StringLength(250, ErrorMessage = "El límite es de 250 caracteres.")]
 	public string Nota { get; set; }
 
 	public bool Eliminado { get; set; } = false;
