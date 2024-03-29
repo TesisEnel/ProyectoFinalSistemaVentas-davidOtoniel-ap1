a7bbb7a86183b5cac2a04092b73f82685dae2d8a
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-29 12:02:55 -0400

    Creación de los services Banco y Capital.

--- SistemaVentas/SistemaVentas/Services/CapitalService.cs
+++ SistemaVentas/SistemaVentas/Services/CapitalService.cs
@@ -1,6 +1,22 @@
-﻿namespace SistemaVentas.Services;
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using SistemaVentas.Data;
+
+namespace SistemaVentas.Services;
 
 public class CapitalService
 {
+	private readonly ApplicationDbContext _contexto;
 
+	public CapitalService(ApplicationDbContext contexto)
+	{
+		_contexto = contexto;
+	}
+
+	public async Task<Capital?> BuscarId(int id)
+	{
+		return await _contexto.Capital
+			.AsNoTracking()
+			.FirstOrDefaultAsync(c => c.CapitalId == id);
+	}
 }
