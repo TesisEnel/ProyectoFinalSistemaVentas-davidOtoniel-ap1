df5c1f85f9048d19fab06591ca3fad39b006e6c8
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-30 09:35:52 -0400

    Modificanción del modelo Cuentas por pagar, ademas de que se agregó el detalle de este modelo.

--- Library/Models/CuentasPorPagar.cs
+++ Library/Models/CuentasPorPagar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,16 @@ public class CuentasPorPagar
 	[Key]
 	public int CuentaPorPagarId { get; set; }
 
-	[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-	public DateTime FechaPago { get; set; } = DateTime.Today;
+	[ForeignKey("Compras")]
+	public int CompraId { get; set; }
+    public Compras Compra { get; set; }
 
-    public decimal Deuda { get; set; }
+	public decimal DeudaInicial { get; set; }
 
-    public decimal Abono { get; set; }
+    public decimal DeudaRestante { get; set; }
 
-    public string Concepto { get; set; }
+    public string Estado { get; set; }
 
-    public int CompraId { get; set; }
+	[ForeignKey("CuentaPorPagarId")]
+	public ICollection<CuentasPorPagarDetalle> CuentasPorPagarDetalle { get; set; } = new List<CuentasPorPagarDetalle>();
 }
