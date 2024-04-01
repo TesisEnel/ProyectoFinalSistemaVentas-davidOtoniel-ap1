9a1ccdae194bf2ad4a73e882630b4f06e688db8d
Stiinger <ortegantony29@gmail.com>
2024-04-01 15:28:02 -0400

    Crear modelo de Cuentas Por Cobrar y su detalle

---
+++ Library/Models/CuentasPorCobrar.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+	public class CuentasPorCobrar
+	{
+		[Key]
+		public int CuentaPorCobrarId { get; set; }
+
+		[ForeignKey("Ventas")]
+		public int VentaId { get; set; }
+		public Ventas Venta { get; set; }
+
+		public decimal DeudaInicial { get; set; }
+
+		public decimal DeudaRestante { get; set; }
+
+		public string Estado { get; set; }
+
+		[ForeignKey("CuentaPorCobrarId")]
+		public ICollection<CuentasPorCobrarDetalle> CuentasPorCobrarDetalle { get; set; } = new List<CuentasPorCobrarDetalle>();
+	}
+}
---
+++ Library/Models/CuentasPorCobrarDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class CuentasPorCobrarDetalle
+{
+	[Key]
+	public int CuentasPorCobrarDetalleId { get; set; }
+
+	[ForeignKey("CuentasPorCobrar")]
+    public int CuentaPorCobrarId { get; set; }
+
+	[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+	public DateTime FechaPago { get; set; } = DateTime.Today;
+
+    public decimal Abono { get; set; }
+
+    public string Concepto { get; set; }
+}
