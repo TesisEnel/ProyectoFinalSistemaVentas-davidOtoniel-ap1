72ce2fd02bdc53fe108c84e87d676fad9bb80d8d
david-o-rosario1201 <finnnn023@gmail.com>
2024-04-08 10:58:43 -0400

    Se creo un modelo PagoDetalle para guardar las fechas y montos a pagar por cada CuentaPorPagar

---
+++ SistemaVentas/SistemaVentas/Data/Migrations/20240408135856_MigracionTemporal2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVentas.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionTemporal2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PagoDetalle",
+                columns: table => new
+                {
+                    PagoDetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CuentaPorPagarId = table.Column<int>(type: "int", nullable: false),
+                    FechaPago = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MontoPago = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagoDetalle", x => x.PagoDetalleId);
+                    table.ForeignKey(
+                        name: "FK_PagoDetalle_CuentasPorPagar_CuentaPorPagarId",
+                        column: x => x.CuentaPorPagarId,
+                        principalTable: "CuentasPorPagar",
+                        principalColumn: "CuentaPorPagarId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PagoDetalle_CuentaPorPagarId",
+                table: "PagoDetalle",
+                column: "CuentaPorPagarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PagoDetalle");
+        }
+    }
+}
